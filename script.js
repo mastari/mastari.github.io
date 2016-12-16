fbf6a7f7a2c62390aa06eb7f6c7cbefe4a497fef
Ari <chocolatepie1000@gmail.com>
2016-12-15 18:13:06 -0600

    Major push

---
+++ script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    var centerH = $(document).width()/3;
+    console.log(centerH + " is the middle brake point");
+    $('.center').css({
+    "margin-left": centerH
+    });
+    var sWidth = $(document).width();
+    console.log(sWidth + " HERE")
+    $("#bar").css({
+        "width": sWidth
+    });
+});
+
