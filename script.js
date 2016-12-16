e61dff94242754d2bd41472b5a4af114a396b641
Ari <chocolatepie1000@gmail.com>
2016-12-15 18:55:24 -0600

    screen padding IOS fix

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ $( document ).ready(function() {
     "margin-left": centerH
     });
     var sWidth = $(document).width();
+    var sHeight = $(document).height();
     console.log(sWidth + " HERE")
     $("#bar").css({
         "width": sWidth
     });
     $("#logo").css({
         "margin-right": sWidth/58
+    });
+    $("#navy li a").css({
+        "padding-top": sWidth/64
+    });
+    $("#navy li a").css({
+        "padding-right": sWidth/91.4285714
+    });
+    $("#navy li a").css({
+        "padding-left": sWidth/91.4285714
+    });
+    $("#navy li a").css({
+        "padding-bottom":  sHeight/42.1538462
     });
 });
 
