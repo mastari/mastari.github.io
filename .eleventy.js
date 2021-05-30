c051f56083824a5ecfb7fbae0f74cd6c02dce8bf
mastari <arikatz.d@gmail.com>
2021-05-30 10:29:35 -0700

    yay

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('style')
   eleventyConfig.addPassthroughCopy('imgs')
+  eleventyConfig.addPassthroughCopy('fonts')
   return {
     passthroughFileCopy: true
   }
